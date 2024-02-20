c63d6d47773e4fc3c35b72d57c572c365aa3aed1
marktlinn <marktlinn@gmail.com>
2024-02-20 21:17:52 +0100

    add: throttler func to mitigate transfer times of goroutines

--- gspot/pipe.go
+++ gspot/pipe.go
@@ -1,6 +1,9 @@
 package gspot
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 // generates requests from the passed function n times, sending the results to the specified (send-only) channel.
 func Produce(out chan<- *http.Request, n int, fn func() *http.Request) {
@@ -9,12 +12,33 @@ func Produce(out chan<- *http.Request, n int, fn func() *http.Request) {
 	}
 }
 
-// runs Produce inside a goroutine enabling concurrency.
+// runs Produce inside a goroutine.
 func produce(n int, fn func() *http.Request) <-chan *http.Request {
 	out := make(chan *http.Request)
 	go func() {
 		defer close(out)
 		Produce(out, n, fn)
+	}()
+	return out
+}
+
+// sets a delay on messages between channels i.e. between what comes in and what goes out.
+func Throttle(in <-chan *http.Request, out chan<- *http.Request, delay time.Duration) {
+	t := time.NewTicker(delay)
+	defer t.Stop()
+
+	for r := range in {
+		<-t.C
+		out <- r
+	}
+}
+
+// runs Throttle inside a goroutine.
+func throttle(in <-chan *http.Request, delay time.Duration) <-chan *http.Request {
+	out := make(chan *http.Request)
+	go func() {
+		defer close(out)
+		Throttle(in, out, delay)
 	}()
 	return out
 }
