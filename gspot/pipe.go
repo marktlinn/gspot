7ddea3b34daba6898152b9427026871c503832e5
marktlinn <marktlinn@gmail.com>
2024-02-21 21:28:28 +0100

    add: splitter func to final part of pipeline

--- gspot/pipe.go
+++ gspot/pipe.go
@@ -2,6 +2,7 @@ package gspot
 
 import (
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -39,6 +40,35 @@ func throttle(in <-chan *http.Request, delay time.Duration) <-chan *http.Request
 	go func() {
 		defer close(out)
 		Throttle(in, out, delay)
+	}()
+	return out
+}
+
+// Splits the pipline up, running the fn func with the data received from in, the results being channeled to out.
+func Split(in <-chan *http.Request, out chan<- *Result, c int, fn SendFunc) {
+	send := func() {
+		for r := range in {
+			out <- fn(r)
+		}
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(c)
+	for ; c > 0; c-- {
+		go func() {
+			defer wg.Done()
+			send()
+		}()
+	}
+	wg.Wait()
+}
+
+// runs the Split function.
+func split(in <-chan *http.Request, c int, fn SendFunc) <-chan *Result {
+	out := make(chan *Result)
+	go func() {
+		defer close(out)
+		Split(in, out, c, fn)
 	}()
 	return out
 }
