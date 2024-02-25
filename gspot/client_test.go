ac8d5be6b6fbb22b54ba78637b4023771231b2a3
marktlinn <marktlinn@gmail.com>
2024-02-25 17:41:29 +0100

    add: simple int tests for client Do method

---
+++ gspot/client_test.go
@@ -0,0 +1,55 @@
+package gspot
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"sync/atomic"
+	"testing"
+)
+
+func TestClientDo(t *testing.T) {
+	t.Parallel()
+
+	const wantRes, WantErrs = 100, 0
+	var (
+		gotRes atomic.Int64
+
+		server = newTestServer(t, func(_ http.ResponseWriter, _ *http.Request) {
+			gotRes.Add(1)
+		})
+
+		req = newRequest(t, http.MethodGet, server.URL)
+	)
+
+	c := Client{
+		C: 1,
+	}
+
+	ttl := c.Do(context.Background(), req, wantRes)
+	if got := gotRes.Load(); got != wantRes {
+		t.Errorf("Res: %d; wanted %d", got, wantRes)
+	}
+	if got := ttl.Requests; got != wantRes {
+		t.Errorf("Reqs: %d; wanted %d", got, wantRes)
+	}
+	if got := ttl.Errors; got != WantErrs {
+		t.Errorf("Errs %d; wanted %d", got, WantErrs)
+	}
+}
+
+func newRequest(t testing.TB, httpMethod, url string) *http.Request {
+	t.Helper()
+	req, err := http.NewRequest(httpMethod, url, http.NoBody)
+	if err != nil {
+		t.Fatalf("Request err=%q; wanted nil", err)
+	}
+	return req
+}
+
+func newTestServer(t testing.TB, handler http.HandlerFunc) *httptest.Server {
+	t.Helper()
+	s := httptest.NewServer(handler)
+	t.Cleanup(s.Close)
+	return s
+}
